02fc8f9e164f31a4d69d4374756748cd60bef597
Steno-95 <lezzistefano95@gmail.com>
2025-04-12 17:32:14 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,25 @@
+function Footer() {
+  return (
+    <footer className="text-center text-[11px] md:text-[15px] text-amber-50 mt-auto">
+      Challenge by{" "}
+      <a
+        className="text-blue-950"
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a
+        className="text-blue-950"
+        href="https://github.com/Steno-95"
+        title="The link will redirect to the github of Stefano Lezzi"
+      >
+        Stefano Lezzi
+      </a>
+      .
+    </footer>
+  );
+}
+
+export default Footer;
